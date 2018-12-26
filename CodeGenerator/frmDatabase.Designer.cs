0ad3edc3c3abc78fa85a9c9310f97c64b4d36c47
叶涛 <821023977@qq.com>
2018-12-26 17:50:54 +0800

    1

--- CodeGenerator/frmDatabase.Designer.cs
+++ CodeGenerator/frmDatabase.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -63,10 +63,27 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(25, 26);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1203, 64);
+            this.groupBox1.Size = new System.Drawing.Size(997, 64);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "SqlServer";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(892, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(755, 21);
+            this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
@@ -77,23 +94,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "ConnectionString";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(113, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(999, 21);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1118, 24);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Go";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button2);
@@ -101,14 +101,14 @@
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Location = new System.Drawing.Point(25, 121);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1203, 64);
+            this.groupBox2.Size = new System.Drawing.Size(997, 64);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "MySql";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1118, 24);
+            this.button2.Location = new System.Drawing.Point(892, 23);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -120,7 +120,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(113, 25);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(999, 21);
+            this.textBox2.Size = new System.Drawing.Size(755, 21);
             this.textBox2.TabIndex = 1;
             // 
             // label2
@@ -139,14 +139,14 @@
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Location = new System.Drawing.Point(25, 217);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(1203, 64);
+            this.groupBox3.Size = new System.Drawing.Size(997, 64);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Sqlite";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1118, 24);
+            this.button3.Location = new System.Drawing.Point(892, 23);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -158,7 +158,7 @@
             // 
             this.textBox3.Location = new System.Drawing.Point(113, 25);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(999, 21);
+            this.textBox3.Size = new System.Drawing.Size(755, 21);
             this.textBox3.TabIndex = 1;
             // 
             // label3
@@ -177,14 +177,14 @@
             this.groupBox4.Controls.Add(this.label4);
             this.groupBox4.Location = new System.Drawing.Point(25, 318);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(1203, 64);
+            this.groupBox4.Size = new System.Drawing.Size(997, 64);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Postgresql";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(1118, 24);
+            this.button4.Location = new System.Drawing.Point(892, 23);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 2;
@@ -196,7 +196,7 @@
             // 
             this.textBox4.Location = new System.Drawing.Point(113, 25);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(999, 21);
+            this.textBox4.Size = new System.Drawing.Size(755, 21);
             this.textBox4.TabIndex = 1;
             // 
             // label4
@@ -215,14 +215,14 @@
             this.groupBox5.Controls.Add(this.label5);
             this.groupBox5.Location = new System.Drawing.Point(25, 420);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(1203, 64);
+            this.groupBox5.Size = new System.Drawing.Size(997, 64);
             this.groupBox5.TabIndex = 4;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Oracle";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(1118, 24);
+            this.button5.Location = new System.Drawing.Point(892, 23);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 2;
@@ -234,7 +234,7 @@
             // 
             this.textBox5.Location = new System.Drawing.Point(113, 25);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(999, 21);
+            this.textBox5.Size = new System.Drawing.Size(755, 21);
             this.textBox5.TabIndex = 1;
             // 
             // label5
@@ -248,7 +248,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(526, 507);
+            this.button6.Location = new System.Drawing.Point(423, 506);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(187, 42);
             this.button6.TabIndex = 5;
@@ -260,7 +260,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1249, 572);
+            this.ClientSize = new System.Drawing.Size(1047, 572);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
