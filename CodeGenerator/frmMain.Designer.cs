fec2b38a540b062e849666ed91e6cc7e4db816b7
znyet <821023977@qq.com>
2018-12-30 10:48:32 +0800

    1

--- CodeGenerator/frmMain.Designer.cs
+++ CodeGenerator/frmMain.Designer.cs
@@ -82,11 +82,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Location = new System.Drawing.Point(5, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 12);
+            this.label2.Size = new System.Drawing.Size(125, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "命名空间(namespace)";
+            this.label2.Text = "namespace or package";
             // 
             // label3
             // 
